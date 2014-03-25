d564bc9cdc5ea488a372d3173677e289b27814ad
Lithrein <lithrein.site@gmail.com>
2014-03-25 20:30:55 +0100

    Cleaning

--- src/dictionary.h
+++
@@ -1,8 +0,0 @@
-#ifndef H_DICTIONARY
-#define H_DICTIONARY
-
-struct tuple_list * load_dictionary (const char *, const char *, struct tuple_list *);
-void clear_dictionaries ();
-
-#endif
-
--- src/levenshtein.h
+++
@@ -1,7 +0,0 @@
-#ifndef H_LEVENSHTEIN
-#define H_LEVENSHTEIN
-
-int levenshtein (char *, char *);
-int damereau_levenstein (char *, char *);
-
-#endif /* H_LEVENSHTEIN */
